584e307de3726f9d8a6b38f7d9704a6d477ec7ce
Shauna Vaynix <shauna@example.com>
2026-03-05 15:22:32 +0800

    Remove ignored files

--- vki/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),  # built-in login/logout
-    path('', include('todo.urls')),      # reservation/dashboard
-    path('pc/', include('pc_login.urls')),  # pc login
-]
